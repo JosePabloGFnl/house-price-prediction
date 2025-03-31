a12269694a581b9e43b56dce06b63d1ad55b680d
Pablo González <jp_glzf@hotmail.com>
2025-03-31 09:57:48 -0600

    Transformations fixed for separated sets

--- transformations.py
+++ transformations.py
@@ -1,38 +1,40 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
 class Transformations:
-    def __init__(self, data, categorical_features, numerical_features):
-        self.data = data
+    def __init__(self, categorical_features, numerical_features):
         self.categorical_features = categorical_features
         self.numerical_features = numerical_features
-
-    def null_handler(self):
-        for col in self.categorical_features:
-            self.data[col].fillna("None", inplace=True)
-
-        for col in self.numerical_features:
-            self.data[col].fillna(0, inplace=True)
-        return self.data
-    
-    def feature_preparation(self):
-        preprocessor = ColumnTransformer(
-        transformers=[
+        self.preprocessor = ColumnTransformer(
+            transformers=[
                 ('num', StandardScaler(), self.numerical_features),
                 ('cat', OneHotEncoder(handle_unknown='ignore', sparse_output=False), self.categorical_features)
             ]
         )
 
-        X_transformed = preprocessor.fit_transform(self.data)
+    def null_handler(self, data):
+        data = data.copy()  # Avoid modifying original DataFrame
+        for col in self.categorical_features:
+            data[col].fillna("None", inplace=True)
+        for col in self.numerical_features:
+            data[col].fillna(0, inplace=True)
+        return data
 
+    def fit_transform(self, data):
+        data = self.null_handler(data)
+        X_transformed = self.preprocessor.fit_transform(data)
         columns = (
             self.numerical_features +
-            list(preprocessor.named_transformers_['cat'].get_feature_names_out(self.categorical_features))
+            list(self.preprocessor.named_transformers_['cat'].get_feature_names_out(self.categorical_features))
         )
+        return pd.DataFrame(X_transformed, columns=columns)
 
-        X_transformed = pd.DataFrame(X_transformed, columns=columns)
-
-        return X_transformed
+    def transform(self, data):
+        data = self.null_handler(data)
+        X_transformed = self.preprocessor.transform(data)
+        columns = (
+            self.numerical_features +
+            list(self.preprocessor.named_transformers_['cat'].get_feature_names_out(self.categorical_features))
+        )
+        return pd.DataFrame(X_transformed, columns=columns)
